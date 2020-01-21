dfc8a4506bcd3bd9c5e918adedbcf914196d90b1
Yang Yan <~@gilgamesh.cc>
2020-01-21 10:47:18 -0500

    multithreading

---
+++ src/main.h
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int APIENTRY wWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR lpCmdLine,
+	int nCmdShow);
+
+int startCEF(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR lpCmdLine,
+	int nCmdShow);
+int startWebserver();
